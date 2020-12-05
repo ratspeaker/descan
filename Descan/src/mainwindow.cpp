b4841a8656c4e3067d1c85b37d3a593cc7b593b5
ratspeaker <jmilivojevic23@gmail.com>
2020-12-05 23:29:57 +0100

    feat: Import button implementation
    Implemented import of images. Image is displayed inside scrollArea.

--- Descan/src/mainwindow.cpp
+++ Descan/src/mainwindow.cpp
@@ -7,6 +7,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    display = new DisplayArea();
+
+    display->getLabel()->resize(0, 0);
+    display->getLabel()->setMouseTracking(true);  //za
+    display->getLabel()->installEventFilter(this); //za crop
+
+    ui->scrollArea->setWidget(display->getLabel());
 }
 
 
@@ -35,19 +42,38 @@ void MainWindow::on_pbBackEdit_clicked()
     ui->stackedWidget->setCurrentIndex(1);
 }
 
-/*
-//otvara se dijalog za odabir slika i prikazuje se u labeli
 void MainWindow::on_pbImport_clicked()
 {
-    m_img->m_filePath = QFileDialog::getOpenFileName(this,tr("Import image"),"/home/",tr("Images (*.png *.xpm *.jpg *.bmp)"));
-    qDebug() << m_img->m_filePath;
 
-    cv::Mat image = m_img->LoadImage();
-    m_img->ScaleImage(image);
-    QPixmap pixmap = m_img->DisplayImage(image);
-    ui->labelImage->setPixmap(pixmap);
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Import Image"), "/home/");
+    if(fileName.isEmpty()){
+        QMessageBox::critical(this, "Choose image", "Can't proceed if image not imported!");
+    }
+    else{
+        ui->pbNextEdit->setDisabled(false);
+    }
+
+    if (!fileName.isEmpty()) {
+         //img = Image();
+         display->setElement(fileName);
+
+         QImage image = display->getElement()->getImage();
+         qDebug() << "dimenzije slike su " << image.size().width() << image.size().height();
+
+         if (image.isNull()) {
+             QMessageBox::information(this, tr("Descan"), tr("Cannot load image."));
+             return;
+         }
+
+         display->setScaleFactor(1.0);
+         setCursor(Qt::ArrowCursor);
+
+         display->setImageInLabel();
+         display->getLabel()->adjustSize();
+    }
 }
 
+/*
 void MainWindow::on_pbImportMultiple_clicked()
 {
     QStringList files = QFileDialog::getOpenFileNames(
@@ -68,10 +94,6 @@ void MainWindow::on_hsBrightness_3_sliderMoved(int position)
 }
 */
 
-void MainWindow::on_pbImport_clicked()
-{
-
-}
 void MainWindow::on_pbImportMultiple_clicked()
 {
 
@@ -92,31 +114,34 @@ void MainWindow::on_toolButton_6_clicked()
 
 }
 
-
+// TODO: treba izmeniti da ne menja direktno sliku(kopija objekta ili naci bolje resenje?)
 void MainWindow::on_hsScale_2_sliderMoved(int position)
 {
-    double resizeFactor = 0.33;
+    double resizeFactor = 0.4;
     if(position)
-        resizeFactor = 0.33 + (3-0.33)*position/100;
+        resizeFactor = 0.4 + (2-0.4)*position/100;
 
-    img->resizeImage(resizeFactor, 's');
+    display->getElement()->resizeImage(resizeFactor, 's');
+    display->setImageInLabel();
 }
 
 void MainWindow::on_hsHorizontal_2_sliderMoved(int position)
 {
-    double resizeFactor = 0.33;
+    double resizeFactor = 0.4;
     if(position)
-        resizeFactor = 0.33 + (3-0.33)*position/100;
+        resizeFactor = 0.4 + (2-0.4)*position/100;
 
-    img->resizeImage(resizeFactor, 'w');
+    display->getElement()->resizeImage(resizeFactor, 'w');
+    display->setImageInLabel();
 }
 
 void MainWindow::on_hsVertical_2_sliderMoved(int position)
 {
-    double resizeFactor = 0.33;
+    double resizeFactor = 0.4;
     if(position)
-        resizeFactor = 0.33 + (3-0.33)*position/100;
+        resizeFactor = 0.4 + (2-0.4)*position/100;
 
-    img->resizeImage(resizeFactor, 'h');
+    display->getElement()->resizeImage(resizeFactor, 'h');
+    display->setImageInLabel();
 }
 
