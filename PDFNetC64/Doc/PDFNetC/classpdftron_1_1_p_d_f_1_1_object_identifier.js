5113840623dc098c3cf9cce3380fd07c42dad570
ratspeaker <jmilivojevic23@gmail.com>
2020-12-13 01:04:02 +0100

    Added library PDFNetC64

---
+++ PDFNetC64/Doc/PDFNetC/classpdftron_1_1_p_d_f_1_1_object_identifier.js
@@ -0,0 +1,22 @@
+var classpdftron_1_1_p_d_f_1_1_object_identifier =
+[
+    [ "Predefined", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088", [
+      [ "e_commonName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a188c18aa0b34ec095dbccaa4dbb0f170", null ],
+      [ "e_surname", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a6cb63e6cb425f44b452b24b04181b850", null ],
+      [ "e_countryName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a100de56454e723ebd01df3d629612b41", null ],
+      [ "e_localityName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088aec1f460dd02d4608163259fadfccbb4f", null ],
+      [ "e_stateOrProvinceName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088afa42f514f86c4af0aa34108d0d1aeb60", null ],
+      [ "e_streetAddress", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a5c0dee96f627f131027229e82e8ee210", null ],
+      [ "e_organizationName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a0d5d94522384a0df968377e8c94eb13b", null ],
+      [ "e_organizationalUnitName", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a650cedf84eec8c6632074c5576c38088a74dfad436c02bfdc2cc2527ca7f887c1", null ]
+    ] ],
+    [ "ObjectIdentifier", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a5e645b57ebcd27d5bf8c810975b224f4", null ],
+    [ "~ObjectIdentifier", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a0eaa371609493fd743ede50bf6ac263d", null ],
+    [ "ObjectIdentifier", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#aa2aa51bce23311afe27d7101de1f10e7", null ],
+    [ "ObjectIdentifier", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a3207b3cd66e77815fffa3097f996affc", null ],
+    [ "ObjectIdentifier", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a5fe611fe0d4f90bfbd48bdbfbae5ea36", null ],
+    [ "Destroy", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#afe1925060d42bea2d4fcb6933efdfefa", null ],
+    [ "GetRawValue", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#a9a01955f7780a6d74f42a6cf2572d576", null ],
+    [ "operator=", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#af1e937f2b88f8692471ddb7d6f295e6f", null ],
+    [ "m_impl", "classpdftron_1_1_p_d_f_1_1_object_identifier.html#ad84661af972a5591a024f85cbcfb326b", null ]
+];
---
+++ PDFNetC64/Doc/PDFNetC/classpdftron_1_1_p_d_f_1_1_timestamping_test_result.js
@@ -0,0 +1,13 @@
+var classpdftron_1_1_p_d_f_1_1_timestamping_test_result =
+[
+    [ "TimestampingTestResult", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a9868267ca4a1a863242af90167b6f652", null ],
+    [ "~TimestampingTestResult", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#aa3500b8f3e835428ba87ee5734c8e429", null ],
+    [ "TimestampingTestResult", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a37a7225f564cd14bff3ccd96109eafc7", null ],
+    [ "Destroy", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#ace81e0547cbcdb41fd2fdf67c41d8f85", null ],
+    [ "GetResponseVerificationResult", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a41dbd6ffd40ea83e4552774f6540e079", null ],
+    [ "GetStatus", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a32e06d268476a37048f90913f331f0d8", null ],
+    [ "GetString", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a69752e769e0bfe446c5c98a9dd6b3c16", null ],
+    [ "HasResponseVerificationResult", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#ab46e3d0380c9116d3448aff400024875", null ],
+    [ "operator=", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#ab5f36dcb3f8ea4cb3f96638d155ac76c", null ],
+    [ "m_impl", "classpdftron_1_1_p_d_f_1_1_timestamping_test_result.html#a2116a436f9dca4eeb8c24747653be941", null ]
+];
